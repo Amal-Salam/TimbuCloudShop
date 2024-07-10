eb96ced0b2ba0f8a79bc06ba2d08b32743ef6a2d
Nana Hauwa Abdulsalam <68066734+Amal-Salam@users.noreply.github.com>
2024-07-10 21:36:22 +0100

    Update cardDetails.jsx

--- e-comm/src/components/checkout/cardDetails.jsx
+++ e-comm/src/components/checkout/cardDetails.jsx
@@ -6,26 +6,41 @@ function CardDetails(){
         <div className='w-[715px] h-[581px] bg-white flex-col justify-start items-center gap-3 inline-flex'>
           <div className='w-[715px] h-[70px]  bg-background_hero rounded-tl-2xl rounded-tr-2xl justify-between items-center inline-flex'>
             <div className="text-stone-300 text-color mx-8 text-lg font-medium font-['Lato']">
-              Card Details
+              Payment
             </div>
           </div>
-{/*           <img className='w-[274px] h-[180px]' src={card} /> */}
+{/*           <img className='w-[274px] h-[180px]' src={card} /> */
           <div className='self-stretch grow shrink basis-0 flex-col justify-start items-start gap-6 flex'>
-            <div className='self-stretch grow shrink basis-0 flex-col justify-between items-start flex'>
+               <div className='w-[715px] h-[82.34px] my-5 justify-start items-center gap-6 inline-flex'>
+          <div className='w-[345.52px]  flex-col justify-start items-start gap-2 inline-flex'>
+            <div className="self-stretch text-zinc-900 text-sm font-medium font-['Lato'] leading-normal">
+              Card Number
+            </div>
+            <input className='self-stretch h-[50px] px-3 py-[18px] bg-zinc-100 rounded-lg justify-start items-center gap-2.5 inline-flex' />
+          </div>
+                    <div className='w-[715px] h-[82.34px] my-5 justify-start items-center gap-6 inline-flex'>
+          <div className='w-[345.52px]  flex-col justify-start items-start gap-2 inline-flex'>
+            <div className="self-stretch text-zinc-900 text-sm font-medium font-['Lato'] leading-normal">
+              Cardholder Name
+            </div>
+            <input className='self-stretch h-[50px] px-3 py-[18px] bg-zinc-100 rounded-lg justify-start items-center gap-2.5 inline-flex' />
+          </div>
+                        
+{/*             <div className='self-stretch grow shrink basis-0 flex-col justify-between items-start flex'>
               <div className='self-stretch p-3 rounded-lg border border-stone-300 justify-start items-center gap-2.5 inline-flex'>
                   <div className="self-stretch text-zinc-900 text-sm font-medium font-['Lato'] leading-normal">
               Card Number
             </div>
-                <input className="text-neutral-600 text-xs font-normal font-['lato']" />
+               <input className='self-stretch h-[50px] px-3 py-[18px] bg-zinc-100 rounded-lg justify-start items-center gap-2.5 inline-flex' />
                 
-              </div>
-              <div className='self-stretch p-3 rounded-lg border border-stone-300 justify-start items-center gap-2.5 inline-flex'>
+              </div> */
+{/*               <div className='self-stretch p-3 rounded-lg border border-stone-300 justify-start items-center gap-2.5 inline-flex'>
                   <div className="self-stretch text-zinc-900 text-sm font-medium font-['Lato'] leading-normal">
               Cardholder Name
             </div>
                 <input className="text-neutral-600 text-xs font-normal font-['lato']"/>
                  
-              </div>
+              </div> */
               <div className='self-stretch justify-start items-center gap-3 inline-flex'>
                 <div className='grow shrink basis-0 h-[39px] p-3 rounded-lg border border-stone-300 justify-start items-center gap-2.5 flex'>
                     <div className="self-stretch text-zinc-900 text-sm font-medium font-['Lato'] leading-normal">
